ebfd3f53b935410a219a11dd0c08fab44f4c3098
Carol Wu <carolyxwu@sina.com>
2020-10-12 20:44:19 +0800

    add test9.m

---
+++ test_file/bit_demapping.m
@@ -0,0 +1,70 @@
+function bit_stream=bit_demapping(syms, L, mapping_conf, ch, ch_conf, sigma)
+% demapping algorithm.
+% Need To: Channel estimation; calculate LLR / L2 distance.
+    mapping_vector = mapping_conf.M;    % Constellation points.
+    bit_per_symbol = mapping_conf.bps;
+    mode = mapping_conf.mode;       % Demapping mode: Channel Estimation mode.
+    alg = mapping_conf.alg;         % Channel Equalization algorithm.
+    output_mode = mapping_conf.out; 
+    % Assume PSK.
+    Amp = abs(mapping_vector(1));
+    SNR = 20*log10(Amp/sigma);
+    
+    if strcmp(mode, 'ch_known')
+        assert(logical(exist('ch', 'var')));
+        if strcmp(alg, 'zf')
+            est_syms=syms ./ ch;
+        elseif strcmp(alg, 'mse')
+            snr = 10^(SNR/10);
+            est_syms=syms .* conj(ch)./(ch.*conj(ch)+1/snr);
+        end
+    end
+
+    if strcmp(mode, 'ch_unknown')
+        pilot_rate = mapping_conf.pilotrate;
+        est_ch = zeros([1,length(syms)]);
+        pilot_flag = false([1,length(syms)]);
+        for k=1:length(syms)
+            if mod(k,pilot_rate) == 1 || k == length(syms)
+                pilot_flag(k) = true;
+                est_ch(k) = syms(k);
+            end
+        end
+        % using linear interp to estimate channels.
+        % est_ch = linear_interp(est_ch, pilot_flag);
+        est_ch = quad_interp(est_ch, pilot_flag, pilot_rate);
+        % est_ch = est_kalman(est_ch, pilot_flag, pilot_rate, ch_conf.rho, ch_conf.b, sigma);
+        if strcmp(alg, 'zf')
+            est_syms = syms ./ est_ch;
+        elseif strcmp(alg, 'mse')
+            snr = 10^(SNR/10);
+            est_syms=conj(est_ch)./(est_ch.*conj(est_ch)+1/snr);
+        end
+        % remove pilots.
+        est_syms = est_syms(~pilot_flag);
+        syms = syms(~pilot_flag);
+    end
+    
+    %% Perform demapping.
+    if strcmp(output_mode, 'hard')
+        L_extended = length(syms) * bit_per_symbol;
+        bit_stream = false([1, L_extended]);
+        % hard-decision.
+        for block = 1:length(est_syms)
+            [~, ind] = min(abs(mapping_vector-est_syms(block)));
+            ind=ind-1;
+            % convert to binary.
+            b_index = bit_per_symbol*(block-1)+1;
+            bit_stream(b_index:b_index+bit_per_symbol-1)=to_binary(ind, ...
+                bit_per_symbol);
+        end
+        bit_stream = bit_stream(1:L);
+    elseif strcmp(output_mode, 'L2')
+        N_constellations = 2^bit_per_symbol;
+        bit_stream = zeros([N_constellations, length(est_syms)]);
+        % using L2 measure.
+        for block=1:length(est_syms)
+            bit_stream(:, block) = abs(mapping_vector-est_syms(block)).^2;
+        end
+    end
+end
--- test_file/test9.m
+++ test_file/test9.m
@@ -1,1 +1,17 @@
-%画误码图案
+%画误码图案 17个信噪比下各画一个，分解映射的和解码的
+%解映射对应encoded_bits和pred_bits
+%解码对应radom_bits_with_crc和decoded_bits_with_crc
+%load('sim_10-Oct-2020 00_56_54_#6.mat')
+setup_mapper;
+SNR_arr = [0, 0.5, 1, 1.5, 2, 2.5, 3, 4, 5, 6, 7, 8, 9, 10, 12.5, 15, 17.5];   % target SNR.
+SNRs_abs = 10.^(SNR_arr/10);
+sigma_arr = sqrt(Ps./SNRs_abs);
+N_sigmas = length(sigma_arr);
+encoded_bits_matrix=zeros(length(encoded_bits),length(SNR_arr));
+pred_bits_matrix=zeros(length(pred_bits),length(SNR_arr));
+parfor sigma_iter = 1:N_sigmas
+    
+    pred_bits = bit_demapping(syms_with_noise, length(encoded_bits), mapping_conf, ch, ch_conf, sigma);
+end
+
+
